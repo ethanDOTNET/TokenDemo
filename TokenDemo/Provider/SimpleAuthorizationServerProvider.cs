8b5517fba31fd2b03d8cfb094c744a97d0d5df08
Frank Wu <blackhao99@gmail.com>
2022-04-29 14:18:08 -0400

    Add project files.

---
+++ TokenDemo/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace TokenDemo
+{
+    internal class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.Validated(new ClaimsIdentity(context.Options.AuthenticationType));
+        }
+    }
+}
